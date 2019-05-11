c681740514b05276f7d2b54a8d6072e4b9e53e0c
Justin Spencer <jspencer.jms@gmail.com>
2019-05-11 14:47:32 -0400

    Meeting boilerplate

--- rails-client/config/routes.rb
+++ rails-client/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index', as: :index
   post '/', to: 'welcome#retrieve_from_api', as: :get_info
 
   post '/change-user', to: 'application#change_user', as: :change_user
+
+  resources :meetings
 end
