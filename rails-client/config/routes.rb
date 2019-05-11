99542f052e1a4dee95b11b78d9e9dc5d3956ace1
Justin Spencer <jspencer.jms@gmail.com>
2019-05-11 14:39:32 -0400

    Boilerplate for swapping users

--- rails-client/config/routes.rb
+++ rails-client/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'welcome#index'
+  get '/', to: 'welcome#index', as: :index
   post '/', to: 'welcome#retrieve_from_api', as: :get_info
+
+  post '/change-user', to: 'application#change_user', as: :change_user
 end
--- rails-client/db/schema.rb
+++ rails-client/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_181944) do
+ActiveRecord::Schema.define(version: 2019_05_11_182127) do
 
   create_table "changesets", force: :cascade do |t|
     t.string "user_id"
